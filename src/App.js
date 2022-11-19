a07c84178774cbbaeba94d6ff9d49328fb42f2ce
Fung624 <109220230+Fung624@users.noreply.github.com>
2022-11-19 17:44:34 +1000

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import './css/fung.css'
+import { New } from "./com/New"
 
-function App() {
+function App () {
+
+  function getAlert () {
+    alert("sasasa")
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='fung'>Fung</div>
+      <New />
+      <New />
+      <New />
+      <New />
+      <button onClick={() => (getAlert())}>deed</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/com/New.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export function New () {
+
+  return (
+    <div>New</div>
+  )
+}
